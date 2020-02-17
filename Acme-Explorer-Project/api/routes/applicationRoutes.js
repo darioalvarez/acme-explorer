d7b4b37251140dcf2dd450279d0eb8566d80d5fb
jorgeron <jorgeron1993@hotmail.com>
2020-02-17 11:13:31 +0100

    Application API routes documentada. Desactivado delete

--- Acme-Explorer-Project/api/routes/applicationRoutes.js
+++ Acme-Explorer-Project/api/routes/applicationRoutes.js
@@ -2,12 +2,34 @@
 module.exports = function(app) {
   var applications = require('../controllers/applicationController');
 
+  /**
+   * Get all applications
+   *    RequiredRoles: Administrator
+   * Post an application 
+   *    RequiredRoles: to be an explorer
+   *
+   * @section applications
+   * @type get post
+   * @url /v1/applications
+  */
   app.route('/v1/applications')
-	.get(applications.list_all_applications)
-	.post(applications.create_an_application);
+	  .get(applications.list_all_applications)
+	  .post(applications.create_an_application);
 
+  /**
+   * Put an application.
+   *    RequiredRoles: to be the application's creator explorer
+   *                   or the trip manager
+   * Get an specific application.
+   *    RequiredRoles: to be the application's creator explorer
+   *                   or the trip manager or an Administrator.
+   * 
+   * @section applications
+   * @type get put delete
+   * @url /v1/applications/:applicationId
+  */
   app.route('/v1/applications/:applicationId')
     .get(applications.read_an_application)
-	.put(applications.update_an_application)
-    .delete(applications.delete_an_application);
+	  .put(applications.update_an_application)
+    //.delete(applications.delete_an_application);
 };
