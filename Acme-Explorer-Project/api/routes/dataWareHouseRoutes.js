882d20c6e6e7bcc311d4a6c10a1d5bf60c66d087
Jorge Roncel <jorgeron1993@hotmail.com>
2020-03-12 21:08:24 +0100

    Verificación de roles de ADMINISTRATOR en routes

--- Acme-Explorer-Project/api/routes/dataWareHouseRoutes.js
+++ Acme-Explorer-Project/api/routes/dataWareHouseRoutes.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function(app) {
   var dataWareHouse = require('../controllers/dataWareHouseController');
-
+  var authController = require('../controllers/authController');
 
   	/**
 	 * Get a list of all indicators or post a new computation period for rebuilding
@@ -16,6 +16,11 @@ module.exports = function(app) {
 	.get(dataWareHouse.list_all_indicators)
 	.post(dataWareHouse.rebuildPeriod);
 
+
+	app.route('/v2/dataWareHouse')
+	.get(authController.verifyUser(['ADMINISTRATOR']), dataWareHouse.list_all_indicators)
+	.post(authController.verifyUser(['ADMINISTRATOR']), dataWareHouse.rebuildPeriod);
+
 	/**
 	 * Get a list of last computed indicator
 	 * RequiredRole: Administrator
@@ -28,11 +33,20 @@ module.exports = function(app) {
 	.get(dataWareHouse.last_indicator);
 
 
+	app.route('/v2/dataWareHouse/latest')
+	.get(authController.verifyUser(['ADMINISTRATOR']), dataWareHouse.last_indicator);
+
 	// Returns he amount of money that explorer has spent on trips during period p, so it needs query params.
     app.route('/v1/cube/:explorer/:period')
-        .get(dataWareHouse.cube)
+		.get(dataWareHouse.cube)
+	
+	app.route('/v2/cube/:explorer/:period')
+        .get(authController.verifyUser(['ADMINISTRATOR']), dataWareHouse.cube)
 
     // Returns the explorers that spent the money returned by the cube, so it needs query params. 
     app.route('/v1/cube/explorers')
-        .get(dataWareHouse.cube_explorers)
+		.get(dataWareHouse.cube_explorers)
+		
+	app.route('/v2/cube/explorers')
+        .get(authController.verifyUser(['ADMINISTRATOR']), dataWareHouse.cube_explorers)
 };
--- Acme-Explorer-Project/api/routes/generalConfigurationRoutes.js
+++ Acme-Explorer-Project/api/routes/generalConfigurationRoutes.js
@@ -1,6 +1,7 @@
 'use strict';
 module.exports = function(app) {
   var generalConfig = require('../controllers/generalConfigurationController');
+  var authController = require('../controllers/authController');
 
 
   /**
@@ -16,10 +17,17 @@ module.exports = function(app) {
   */
   app.route('/v1/generalConfiguration')
 	  .get(generalConfig.get_general_configuration)
-      .post(generalConfig.create_general_configuration);
+    .post(generalConfig.create_general_configuration);
+
+  app.route('/v2/generalConfiguration')
+	  .get(authController.verifyUser(['ADMINISTRATOR']), generalConfig.get_general_configuration)
+    .post(authController.verifyUser(['ADMINISTRATOR']), generalConfig.create_general_configuration);
 
     
   app.route('/v1/generalConfiguration/:idGeneralConfiguration')
     .put(generalConfig.update_general_configuration);
+
+  app.route('/v2/generalConfiguration/:idGeneralConfiguration')
+    .put(authController.verifyUser(['ADMINISTRATOR']), generalConfig.update_general_configuration);
 
 }
