41d8d0f849bc4d9182c4bbbd4e82b5b89c6bd428
Carlos Espinoza <carlosespinoza@MBP-de-Carlos.technicolor.net>
2020-03-16 14:22:30 +0100

    Adding v2 routes for sponsorships

--- Acme-Explorer-Project/api/controllers/sponsorshipController.js
+++ Acme-Explorer-Project/api/controllers/sponsorshipController.js
@@ -3,6 +3,7 @@
 /*---------------Sponsorship----------------------*/
 var mongoose = require('mongoose'),
   Sponsorship = mongoose.model('Sponsorships');
+  var auth = require('./authController');
 
 exports.list_all_sponsorships = function(req, res) {
   //Check if the user is an administrator and if not: res.status(403); "an access token is valid, but requires more privileges"
@@ -16,34 +17,84 @@ exports.list_all_sponsorships = function(req, res) {
   });
 };
 
-
 exports.create_an_sponsorship = function(req, res) {
   //Check if the user is an administrator and if not: res.status(403); "an access token is valid, but requires more privileges"
-  var new_sponsorship = new Sponsorship(req.body);
-  new_sponsorship.save(function(err, sponsorship) {
-    if (err){
-      if(err.name=='ValidationError') {
-          res.status(422).send(err);
+    var new_sponsorship = new Sponsorship(req.body);
+    new_sponsorship.save(function(err, sponsorship) {
+      if (err){
+        if(err.name=='ValidationError') {
+            res.status(422).send(err);
+        }
+        else{
+          res.status(500).send(err);
+        }
       }
       else{
-        res.status(500).send(err);
+        res.json(sponsorship);
       }
-    }
-    else{
-      res.json(sponsorship);
-    }
+    });
+};
+
+exports.create_an_sponsorship_v2 = function(req, res) {
+  //Check if the user is an administrator and if not: res.status(403); "an access token is valid, but requires more privileges"
+  auth.verifyUser(['Sponsor'])(req, res, (error, actor) => {
+    var new_sponsorship = new Sponsorship(req.body);
+    new_sponsorship.save(function(err, sponsorship) {
+      if (err){
+        if(err.name=='ValidationError') {
+            res.status(422).send(err);
+        }
+        else{
+          res.status(500).send(err);
+        }
+      }
+      else{
+        res.json(sponsorship);
+      }
+    });
   });
 };
 
+exports.update_an_sponsorship_v2 = function(req, res) {
+  //Check that the user is administrator if it is updating more things than comments and if not: res.status(403); "an access token is valid, but requires more privileges"
+  auth.verifyUser(['Sponsor'])(req, res, (error, actor) => {
+  Sponsorship.findOneAndUpdate({_id: req.params.sponsorshipId}, req.body, {new: true}, function(err, sponsorship) {
+      if (err){
+        if(err.name=='ValidationError') {
+            res.status(422).send(err);
+        }
+        else{
+          if (sponsorship) {
+            if(sponsorship.sponsorId != actor._id) {
+                res.status(401).send({ err: dict.get('Unauthorized', lang) })
+                return;
+            }
+          }
+          res.status(500).send(err);
+        }
+      }
+      else{
+        res.json(sponsorship);
+      }
+    });
+  });
+};
 
 exports.change_flat_rate = function(req, res) {
-  res.send('Not implemented yet');
+  var currSponsorship = req.body;
+	Sponsorship.findById(currSponsorship._id, function(err, sponsorship) {
+		sponsorship.update(
+		    { _id: currSponsorship._id },
+		    {
+		      $set: {
+		        cost: currSponsorship.cost,
+		      },
+		    }
+		);
+    
+  });
 };
 
-exports.consult_cube = function(req, res) {
-  console.log('Searching an sponsorship depending on params');
-  res.send('Not implemented yet');
-};
 
 
 exports.read_an_sponsorship = function(req, res) {
@@ -57,6 +108,18 @@ exports.read_an_sponsorship = function(req, res) {
     });
 };
 
+exports.pay_a_sponsorship = function(req, res) {
+  auth.verifyUser(['Sponsor'])(req, res, (error, actor) => {
+    Sponsorship.findOneAndUpdate({_id: req.params.sponsorshipId},  { $set: {"payed": "true" }}, {new: true}, function(err, sponsorship) {
+      if (err){
+        res.status(500).send(err);
+      }
+      else{
+        res.json(sponsorship);
+      }
+    });
+  });
+};
 
 exports.update_an_sponsorship = function(req, res) {
   //Check that the user is administrator if it is updating more things than comments and if not: res.status(403); "an access token is valid, but requires more privileges"
@@ -77,11 +140,32 @@ exports.update_an_sponsorship = function(req, res) {
 
 exports.delete_an_sponsorship = function(req, res) {
   //Check if the user is an administrator and if not: res.status(403); "an access token is valid, but requires more privileges"
+  auth.verifyUser(['Sponsor'])(req, res, (error, actor) => {
     Sponsorship.deleteOne({_id: req.params.sponsorshipId}, function(err, sponsorship) {
         if (err){
             res.status(500).send(err);
         }
         else{
+            res.json({ message: 'Sponsorship successfully deleted' });
+        }
+    });
+  });
+
+};
+
+exports.delete_an_sponsorship_v2 = function(req, res) {
+  //Check if the user is an administrator and if not: res.status(403); "an access token is valid, but requires more privileges"
+    Sponsorship.deleteOne({_id: req.params.sponsorshipId}, function(err, sponsorship) {
+        if (err){
+            res.status(500).send(err);
+        }
+        else{
+          if (sponsorship) {
+            if(sponsorship.sponsorId != actor._id) {
+                res.status(401).send({ err: dict.get('Unauthorized', lang) })
+                return;
+            }
+          }
             res.json({ message: 'Sponsorship successfully deleted' });
         }
     });
--- Acme-Explorer-Project/api/routes/sponsorshipRoutes.js
+++ Acme-Explorer-Project/api/routes/sponsorshipRoutes.js
@@ -1,6 +1,7 @@
 'use strict';
 module.exports = function(app) {
   var sponsorships = require('../controllers/sponsorshipController');
+  var auth = require('../controllers/authController');
 
   /**
    * Manage catalogue of sponsorships: 
@@ -18,27 +19,15 @@ module.exports = function(app) {
     .post(sponsorships.create_an_sponsorship);
 
   /**
-   * get results of the cube consult
-   *    RequiredRoles: Sponsor
-   * 
-   * @section sponsorships
-	 * @type get
-	 * @url /v1/sponsorships/cube
-   */
-  app.route('/v1/sponsorships/cube')
-  .get(sponsorships.consult_cube)
-
-
-  /**
    * Manage flat rate cost 
    * Post number
    *    RequiredRoles: Administrator
    *
    * @section sponsorships
    * @type put 
-   * @url /v1/sponsorships/flatRate
+   * @url /v1/sponsorships/:sponsorshipId/flatRate
   */
-  app.route('/v1/sponsorships/flatRate')
+  app.route('/v1/sponsorships/:sponsorshipId/flatRate')
   .put(sponsorships.change_flat_rate)
 
 
@@ -59,5 +48,60 @@ module.exports = function(app) {
 	  .put(sponsorships.update_an_sponsorship)
     .delete(sponsorships.delete_an_sponsorship);
   
-  
+  /**
+   * Manage flat rate cost 
+   * Post number
+   *    RequiredRoles: Administrator
+   *
+   * @section sponsorships
+   * @type put 
+   * @url /v1/sponsorships/:sponsorshipId/flatRate
+  */
+  app.route('/v2/sponsorships/:sponsorshipId/flatRate')
+      .patch(auth.verifyUser(['Administrator']), sponsorships.change_flat_rate)
+
+ /**
+   * Manage catalogue of sponsorships: 
+   * Post sponsorships
+   *    RequiredRoles: Administrator
+   * Get sponsorships 
+   *    RequiredRoles: Administrator
+   *
+   * @section sponsorships
+   * @type put 
+   * @url /v2/sponsorships
+  */
+ app.route('/v2/sponsorships')
+ .get(sponsorships.list_all_sponsorships)
+ .post(sponsorships.create_an_sponsorship_v2);
+
+
+   /**
+   * Put comments on an sponsorship or update it
+   *    RequiredRoles: any (comment); administrator if any other update
+   * Delete an sponsorship
+   *    RequiredRoles: Administrator
+   * Get an sponsorship
+   *    RequiredRoles: any
+   * 
+   * @section sponsorships
+   * @type get put delete 
+   * @url /v1/sponsorships/:sponsorshipId
+  */
+ app.route('/v2/sponsorships/:sponsorshipId')
+ .get(sponsorships.read_an_sponsorship)
+ .put(sponsorships.update_an_sponsorship_v2)
+ .delete(sponsorships.delete_an_sponsorship);
+
+   /**
+   * Manage the play status
+   * Post number
+   *    RequiredRoles: Administrator
+   *
+   * @section sponsorships
+   * @type put 
+   * @url /v1/sponsorships/:sponsorshipId/flatRate
+  */
+  app.route('/v1//sponsorships/:sponsorshipId/pay')
+    .put(sponsorships.pay_a_sponsorship);
 };
